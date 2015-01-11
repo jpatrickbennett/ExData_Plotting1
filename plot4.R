09ae0fdea1fb6bd5b82bb484cfbd127a026a9c48
jpatrickbennett <patrick@elderfields.com>
2015-01-11 15:50:51 -0500

    Update 2
    added comments to plot4.r

--- plot4.R
+++ plot4.R
@@ -13,15 +13,20 @@ data <- subset(data, dateTime >= as.POSIXct('2007-02-01') & dateTime < as.POSIXc
 #create the plot as a PNG file.
 png(filename = "plot4.png", height = 480, width = 480, units = "px")
 par(mfcol=c(2,2))
+
+#first plot
 plot(data$Global_active_power ~ data$dateTime, type = "l", xlab="", ylab="Global Active Power")
 
+#second plot
 plot(data$Sub_metering_1 ~ data$dateTime, type ="n", xlab="", ylab="Energy sub metering")
 points(data$Sub_metering_1 ~ data$dateTime, type="l")
 points(data$Sub_metering_2 ~ data$dateTime, type = "l", col = "red")
 points(data$Sub_metering_3 ~ data$dateTime, type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),  bty="n", lty = c(1,1,1), col=c("black","red","blue"))
 
+#third plot
 plot(data$Voltage ~ data$dateTime, type = "l", ylab = "Voltage", xlab="datetime")
 
+#fourth plot
 plot(data$Global_reactive_power ~ data$dateTime, type = "l", ylab = "Global_reactive_power", xlab="datetime")
 dev.off()
